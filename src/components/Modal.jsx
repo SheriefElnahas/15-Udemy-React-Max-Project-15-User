f7bc2cb216ea5cd0e0c59a432d5d86315cfe4ff3
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-02-20 06:44:55 +0200

    Modal React Portal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import ReactDOM from 'react-dom';
 import './Modal.css';
 
 function Modal({ onCloseModal, error }) {
-  return (
+  return ReactDOM.createPortal(
     <div className="modal-backdrop">
       <div className="modal">
         <h1>Invalid Input</h1>
         <p>{error}</p>
         <button onClick={() => onCloseModal()}>Okay</button>
       </div>
-    </div>
+    </div>,
+    document.body
   );
 }
 
